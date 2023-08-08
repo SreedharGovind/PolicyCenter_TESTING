31ebb6dedac2bc808c7f20fc528b6685c677da25
SreedharGovind <140377328+SreedharGovind@users.noreply.github.com>
2023-08-08 15:00:52 +0530

    ExcelCredentialsClass Created

---
+++ src/test/java/PolicyCenterTransactions/ExcelUtilCredentials.java
@@ -0,0 +1,37 @@
+package PolicyCenterTransactions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import PolicyCenterTransactions.CommonClass;
+
+public class ExcelUtilCredentials {
+	public static String getCellValueByLabel(String label) throws IOException {
+
+		FileInputStream file = new FileInputStream(new File(CommonClass.excelFilePath));
+		String sheetName = "Credentials";
+		Workbook workbook = WorkbookFactory.create(file);
+
+		Sheet sheet = workbook.getSheet(sheetName);
+
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				if (cell.getStringCellValue().equals(label)) {
+					String value = row.getCell(cell.getColumnIndex() + 1).getStringCellValue();
+					workbook.close();
+					return value;
+				}
+			}
+		}
+		workbook.close();
+		return null;
+	}
+
+}
